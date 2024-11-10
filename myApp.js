569d296aba5080acb256717dd26b016b932f1005
jkenjarCR d <jkenjar@clubready.com>
2024-11-10 00:06:17 +0000

    A

--- myApp.js
+++ myApp.js
@@ -1,14 +1,6 @@
-let express = require('express');
-let app = express();
+// let express = require('express');
+// let app = express();
 
 console.error("Hello World");
 
-// app.get('/test-log', (req, res) => {
-//     var home_page = __dirname + '/views/index.html';
-//     res.send('Hello World');
-//     res.sendFile(home_page);
-// });
-
-// app.listen(3000);
-
-module.exports = app;
+// module.exports = app;
